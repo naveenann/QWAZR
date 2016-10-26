bb7c8f477f824e8855013162a89386b9ef913d7b
Emmanuel Keller <ekeller@open-search-server.com>
2016-10-26 15:17:34 +0200

    Fixes java.nio.file.NoSuchFileException on StoreSchemaInstance.java:148

--- qwazr-store/src/main/java/com/qwazr/store/StoreSchemaInstance.java
+++ qwazr-store/src/main/java/com/qwazr/store/StoreSchemaInstance.java
@@ -141,10 +141,10 @@ class StoreSchemaInstance implements Closeable {
 				if (files != null && files.length > 0)
 					throw new ServerException(Response.Status.NOT_ACCEPTABLE, "The directory is not empty");
 			}
+			final Path parent = path.getParent();
 			if (!Files.deleteIfExists(path) && Files.exists(path))
 				throw new ServerException(Response.Status.INTERNAL_SERVER_ERROR,
 						"Unable to delete the file: " + relativePath);
-			final Path parent = path.getParent();
 			if (Files.isSameFile(parent, dataDirectory))
 				return path;
 			final String[] parentFiles = parent.toFile().list();
